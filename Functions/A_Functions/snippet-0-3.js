8ec7ad86076c31e8f45a0ef8cee9649f97d644b8
Chuma Wakaba <cwakaba73@gmail.com>
2023-05-08 10:35:38 +0200

    Update

--- Functions/A_Functions/snippet-0-3.js
+++ Functions/A_Functions/snippet-0-3.js
@@ -1,14 +1,14 @@
 // snippet 0-3
 let average = function (num1, num2) {
-    console.log("calculating...");
+    console.log("calculating..."); 
     return (num1 + num2) / 2;
   };
   
-  console.log(average(5, 10));
-  console.log(average(20, 26));
-  console.log(average(50, 100) + 2);
+  console.log(average(5, 10)); //calculating...7.5
+  console.log(average(20, 26)); //calculating...23
+  console.log(average(50, 100) + 2); //calculating...77
   
   let a = 21 + 3;
   let b = 20;
   let n = average(a, b);
-  console.log(average(n, 18));
+  console.log(average(n, 18));  //calculating...20
