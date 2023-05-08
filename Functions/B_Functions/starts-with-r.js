e8ebe46774a47c81a9338b7302b8ff335d6550a4
Chuma Wakaba <cwakaba73@gmail.com>
2023-05-08 11:35:21 +0200

    Update

---
+++ Functions/B_Functions/starts-with-r.js
@@ -0,0 +1,18 @@
+// Write a function `startsWithR` that accepts a string as an argument and returns a boolean indicating
+// whether or not the string starts with 'r' or 'R'.
+
+function startsWithR(str)
+{
+    if (str[0] === "R" || str[0] === "r")
+    {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(startsWithR("roger that")); // true
+console.log(startsWithR("Row, row, row your boat")); // true
+console.log(startsWithR("slip")); // false
+console.log(startsWithR("Taxicab")); // false
