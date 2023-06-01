8680b10b9d4d45e21807ce1adb29afa2eaff0c58
Chuma Wakaba <cwakaba73@gmail.com>
2023-06-01 15:07:08 +0200

    Practise

--- Arrays/A_Arrays/filter-long-words.js
+++ Arrays/A_Arrays/filter-long-words.js
@@ -1,21 +1,22 @@
+// Write a function `filterLongWords` that accepts an array of strings as an argument. The function
+// should return a new array containing only the strings that are less than 5 characters long.
+
 function filterLongWords(strArr)
 {
     let newArr = [];
 
-    for (i = 0; i < strArr.length; i++)
+    for(i = 0; i < strArr.length; i++)
     {
-        let element = strArr[i]; 
-        if(element.length < 5)
+        let word = strArr[i];
+        if(word.length < 5)
         {
-            newArr.push(element);
+            newArr.push(word);
         }
     }
-
     return newArr;
 }
 
 console.log(filterLongWords(["kale", "cat", "retro", "axe", "heirloom"]));
 // ['kale', 'cat', 'axe']
-
 console.log(filterLongWords(["disrupt", "pour", "trade", "pic"]));
 // ['pour', 'pic']
