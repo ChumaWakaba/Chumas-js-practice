47908dd497c6b26211b6e34af16fa046e61715ab
Chuma Wakaba <cwakaba73@gmail.com>
2023-06-01 14:32:53 +0200

    Practise

--- Arrays/A_Arrays/bleep-vowels.js
+++ Arrays/A_Arrays/bleep-vowels.js
@@ -24,27 +24,3 @@ console.log(bleepVowels("skateboard")); // 'sk*t*b**rd'
 console.log(bleepVowels("slipper")); // 'sl*pp*r'
 console.log(bleepVowels("range")); // 'r*ng*'
 console.log(bleepVowels("brisk morning")); // 'br*sk m*rn*ng'
-
-
-// function bleepVowels(str)
-// {
-//     let newChar ="";
-//     for (i = 0; i < str.length; i++)
-//     {
-//         let element = str[i];
-//         if (element === "a" || element === "e" || element === "i" || element === "o" || element === "u")
-//         {
-//             newChar = newChar + "*";
-//         }
-//         else {
-//             newChar = newChar + element;
-//         }
-//     }
-//     return newChar;
-   
-// }
-
-// console.log(bleepVowels("skateboard")); // 'sk*t*b**rd'
-// console.log(bleepVowels("slipper")); // 'sl*pp*r'
-// console.log(bleepVowels("range")); // 'r*ng*'
-// console.log(bleepVowels("brisk morning")); // 'br*sk m*rn*ng'
