0dcfe849fc2d6c8d118dced5db0147d7f4f0eef1
Chuma Wakaba <cwakaba73@gmail.com>
2023-06-07 14:00:11 +0200

    Practise

---
+++ Arrays/B_Arrays/your-average-function.js
@@ -0,0 +1,30 @@
+// Write a function `yourAverageFunction` that accepts an array of numbers as an argument. The
+// function should return the average of all elements of the array. If the input array is empty,
+// then the function should return null.
+
+function yourAverageFunction(numArr)
+{
+    let avg;
+    let sum = 0;
+    for(i = 0; i < numArr.length; i++)
+    {
+        let element = numArr[i];
+        
+            sum = sum + element;
+    }
+
+    avg = sum / numArr.length;
+
+    if (sum > 0)
+    {
+        return avg;
+    }
+    else{
+        return null;
+    }
+}
+
+console.log(yourAverageFunction([5, 2, 7, 24])); // 9.5
+console.log(yourAverageFunction([100, 6])); // 53
+console.log(yourAverageFunction([31, 32, 40, 12, 33])); // 29.6
+console.log(yourAverageFunction([])); // null
